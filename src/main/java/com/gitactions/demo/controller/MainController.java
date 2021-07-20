e60fb3bb05829b5f043e0423304250695e2326e8
sdnafiya6@outlook.com <Nafiya6>
2021-07-20 14:21:19 +0530

    Added controller changes

--- src/main/java/com/gitactions/demo/controller/MainController.java
+++ src/main/java/com/gitactions/demo/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
     
-    @GetMapping(path = "/getInfo") 
+    @GetMapping(path = "/") 
     public String getInfo() {
         return "Welcome to Spring Agular demo";
     }
